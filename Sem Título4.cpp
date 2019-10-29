f622b718967a7132f2ef272bfe8d475fa4ab5cf5
Ana <quezialima@live.com>
2019-10-29 23:53:57 +0800

    Projeto criado

---
+++ Sem Título4.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+//jhkjjn.,
+main()
+{
+int num,c;
+
+printf("Cubo de um numero\n\n");
+printf("Digite o numero:");
+/*
+
+bjhybgnbkl.
+*/
+
+scanf("%d",&num);
+
+c = num*num*num;
+
+printf("\nCubo de %d = %d",num,c);
+
+getche();
+}
+
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float a;
+int	x = 1;
+	int y = 3;
+	a = 7.5;
+	int mat = 123;
+	int idade = 25;
+	printf ("O valor de a = %.1f", a);
+	printf ("\n\nResultado da soma:%d tem %d anos", mat, idade);
+	printf("\n\nResultado da soma: %d", x+y);
+	
+    return 0;
+}
